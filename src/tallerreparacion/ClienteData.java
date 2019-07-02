f5ed94a2148ff9712a732230b64bce58042dfc3d
Lucasm54 <lucasm5484@gmail.com>
2019-07-02 04:42:07 +0200

    subi cliente data

--- src/tallerreparacion/ClienteData.java
+++ src/tallerreparacion/ClienteData.java
@@ -5,10 +5,99 @@
  */
 package tallerreparacion;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Lucas
  */
 public class ClienteData {
     
+    private Connection conexion;
+
+    public CienteData(Conexion con) {
+        try{
+            conexion = con.getConnection();
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"No se encuentra la conexion");
+        }
+    }
+    
+    public void guardarAparato(Cliente cliente){
+        try {
+            String sql = "INSERT INTO aparato(nombre_cliente , dni , domicilio , celular) VALUES(? ,? ,? ,?);";
+            PreparedStatement ps;
+            ps = conexion.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, cliente.getNombreCliente());
+            ps.setString(2, cliente.getDni());
+            ps.setString(3, cliente.getDomicilio());
+            ps.setString(4, cliente.getCelular());
+            
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                cliente.setIdCliente(rs.getInt(1));
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Nose puedo obtener el id");
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se puede guardar cliente");
+        }
+    }
+    
+    public void borrarCliente(int idCliente){
+            try{
+                //CONSULTA DE SQL
+                String sql = "DELETE FROM aparato WHERE id_cliente=?";
+                //RETORNA EL ID AUTOINCREMENTAL
+                PreparedStatement ps;
+                ps = conexion.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+                //SETEO EL ID
+                ps.setInt(1, idCliente);
+                //ELIMINO CON EL METODO EXECUTEuPDATE
+                ps.executeUpdate();
+
+
+                ps.close();
+            }
+            catch(SQLException ex){
+                System.out.println("Error al eliminar cliente" + ex.getMessage());
+            }
+    }
+    
+    public void actualizarCliente(Cliente cliente){
+        try{
+            String sql = "UPDATE aparato SET nombre_cliente = ?, dni= ? , domicilio = ? ,celular = ? WHERE id_cliente = ?;";
+            
+            PreparedStatement ps;
+            ps = conexion.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1,cliente.getNombreCliente());
+            ps.setString(2,cliente.getDni());
+            ps.setString(3,cliente.getDomicilio());
+            ps.setString(4,cliente.getCelular());
+            
+            
+            
+            ps.executeUpdate();
+            
+            ps.close();
+        }
+        catch(SQLException ex){
+            System.out.println("Error al actualizar cliente" + ex.getMessage());
+        }
+    }
+        
 }
+
